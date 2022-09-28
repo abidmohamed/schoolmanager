b96bc8551d5b103df2ec297725b960f0c774ddb3
Nadhir <abidmohamednadhir@gmail.com>
2022-09-28 11:31:41 +0100

    fix:fixing the static file not loading bug

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -191,7 +191,7 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Uncomment if you have extra static files and a directory in your GitHub repo.
 # If you don't have this directory and have this uncommented your build will fail
