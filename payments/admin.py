f9788d198d6ef66aa3a2266ed50ecda815ee8068
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2022-12-24 15:59:43 +0100

    fea: add payments to admin panel

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from payments.models import ParentPayment
+
+
+@admin.register(ParentPayment)
+class ParentPaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'parent', 'amount', 'pay_date')
