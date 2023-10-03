2e75f1ad5e19f6d55b40709a3fcac2365f34893d
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2023-10-03 11:05:05 +0100

    fea:teacher in admin panel

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from teacher.models import Teacher
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    fields = ['user', 'profile', 'subject'
+        , 'name', 'address', 'phone', 'email'
+        , 'major', 'certificate', 'salary'
+        , 'birthday', 'is_active', 'date_joined'
+              ]
